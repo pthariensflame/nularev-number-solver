41de942c0791eaf6ee1437192648687a4ddb246f
Documenter.jl <documenter@juliadocs.github.io>
2025-01-11 20:32:26 +0000

    build based on 33a5b24

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NularevNumberSolver","category":"page"},{"location":"#NularevNumberSolver","page":"Home","title":"NularevNumberSolver","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NularevNumberSolver.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NularevNumberSolver]","category":"page"}]
+}
